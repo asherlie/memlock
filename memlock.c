4fdc8342b00e1b0c8cac3f61f4900b57bcf5a859
asherlie <lieber.asher@gmail.com>
2018-07-14 02:57:00 -0400

    locks are now removed when enter is pressed

--- memlock.c
+++ memlock.c
@@ -1,14 +1,14 @@
 #include "vmem_access.h"
 
-int main(int a, char** b){
-      if(a < 4)return -1;
-      pid_t pid = atoi(b[1]);
-      void* addr = (void*)strtoul(b[2], NULL, 16);
-      int v = atoi(b[3]);
+int main(int argc, char* argv[]){
+      if(argc < 4)return -1;
+      pid_t pid = atoi(argv[1]);
+      void* addr = (void*)strtoul(argv[2], NULL, 16);
+      int v = atoi(argv[3]);
       struct lock_container lc;
-      lock_container_init(&lc, 1);
+      lock_container_init(&lc, 5);
       create_lock(&lc, pid, &addr, &v, NULL, 1, false, true, NULL);
-      printf("address %p in proccess %i locked to %i\nlock has pid: %i\npress enter to remove lock", addr, pid, v, lc.locks[0].pid);
+      printf("address %p in proccess %i locked to %i\nlock has pid: %i\npress enter to remove locks", addr, pid, v, lc.locks[0].pid);
       while(getchar() != '\n');
-      /*free_locks(&lc);*/
+      free_locks(&lc);
 }
