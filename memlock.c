4ce36ea7ae63bcec4df312d7a0d7c147aeb38b4f
asherlie <lieber.asher@gmail.com>
2018-07-14 03:29:08 -0400

    simplified logic, improved duplicate check logic

--- memlock.c
+++ memlock.c
@@ -1,7 +1,7 @@
 #include "vmem_access.h"
 
 int main(int argc, char* argv[]){
-      bool ps = (argc < 2) ? false : true;
+      bool ps = argc >= 2;
       pid_t pid;
       if(ps)pid = atoi(argv[1]);
       void* addr; void* pa = 0x0;
@@ -18,12 +18,12 @@ int main(int argc, char* argv[]){
                   puts("enter addr, val");
                   fscanf(stdin, "%p %i", &addr, &val);
             }
-            if(pa != addr && pv != val){
+            if(pa != addr || pv != val){
                   create_lock(&lc, pid, &addr, &val, NULL, 1, false, true, NULL);
                   printf("address %p in proccess %i locked to %i\n", addr, pid, val);
             }
             pa = addr; pv = val;
       }while(getchar() != 'q');
-      free_locks(&lc);
+      printf("%i locks have been removed\n", free_locks(&lc));
       return 0;
 }
