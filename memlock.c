c80e47e4a7a3c2c874b2622303ae1eed2cae8212
asherlie <lieber.asher@gmail.com>
2018-09-16 15:00:02 -0400

    fixed seg fault that occured when int locks were added after string locks, added -v flag

--- memlock.c
+++ memlock.c
@@ -1,6 +1,8 @@
 #include <vmem_access.h>
 #include <string.h>
 
+#define MEMLOCK_VER "1.0.0"
+
 bool strtoi(const char* str, int* i){
       char* res;
       int r = (int)strtol(str, &res, 10);
@@ -24,6 +26,10 @@ bool has_ac(pid_t pid){
 }
 
 int main(int argc, char* argv[]){
+      if(argc >= 2 && !strncmp(argv[1], "-v", 3)){
+            printf("memlock %s using libmemcarve %s\n", MEMLOCK_VER, MEMCARVE_VER);
+            return 0;
+      }
       bool ir = false;
       if(!(ir = has_ac(1)))fprintf(stderr, "WARNING: root permissions are required\n");
       pid_t pid;
@@ -33,7 +39,10 @@ int main(int argc, char* argv[]){
             if(ir)fprintf(stderr, "WARNING: invalid pid entered, starting memlock in any-pid mode\n");
             else fprintf(stderr, "WARNING: pid entered may be invalid, starting memlock in any-pid mode\n");
       }
-      void* addr = 0x0; void* pa = 0x0;
+      void** addr = malloc(sizeof(void*));
+      *addr = 0x0;
+      void* pa = *addr;
+      int* val_p = NULL;
       int val = 0, pv = 0;
       struct lock_container lc;
       lock_container_init(&lc, 5); 
@@ -49,7 +58,7 @@ int main(int argc, char* argv[]){
                         break;
                   }
                   fscanf(stdin, "%s %s", addr_s, val_s);
-                  if(!strtoi(pid_s, &pid) || !strtop(addr_s, &addr) || addr == 0x0 || !has_ac(pid))continue;
+                  if(!strtoi(pid_s, &pid) || !strtop(addr_s, addr) || *addr == 0x0 || !has_ac(pid))continue;
             }
             else{
                   puts("enter addr, val");
@@ -59,27 +68,34 @@ int main(int argc, char* argv[]){
                         break;
                   }
                   fscanf(stdin, "%s", val_s);
-                  if(!strtop(addr_s, &addr) || addr == 0x0)continue;
+                  if(!strtop(addr_s, addr) || *addr == 0x0)continue;
             }
             integers = strtoi(val_s, &val);
-            if(pa != addr || pv != val){
+            if(pa != *addr || pv != val){
                   #pragma GCC diagnostic ignored "-Wmaybe-uninitialized"
-                  char* vs = NULL;
+                  char** vs = NULL;
                   if(!integers){
                         // length will never be > than that of val_s
-                        vs = malloc(sizeof(val_s));
-                        memcpy(vs, val_s, sizeof(val_s));
+                        vs = malloc(sizeof(char*));
+                        *vs = malloc(sizeof(val_s));
+                        memcpy(*vs, val_s, sizeof(val_s));
                   }
-                  create_lock(&lc, pid, &addr, &val, &vs, 1, false, integers);
-                  if(integers)printf("address %p in proccess %i locked to %i\n", addr, pid, val);
-                  else printf("address %p in proccess %i locked to \"%s\"\n", addr, pid, val_s);
+                  else{
+                        val_p = malloc(sizeof(int));
+                        *val_p = val;
+                  }
+                  create_lock(&lc, pid, addr, val_p, vs, 1, false, integers);
+                  if(integers)printf("address %p in proccess %i locked to %i\n", *addr, pid, val);
+                  else printf("address %p in proccess %i locked to \"%s\"\n", *addr, pid, val_s);
             }
-            pa = addr; pv = val;
+            pa = *addr; pv = val;
+            addr = malloc(sizeof(void*));
       }
+      free(addr);
       if(ch == 'q'){
             puts("removing the following locks:");
             print_locks(&lc);
-            printf("%i locks have been removed\n", free_locks(&lc, 0));
+            printf("%i locks have been removed\n", free_locks(&lc, 3));
       }
       else if(lc.n > 0){
             unsigned int n = lc.n;
